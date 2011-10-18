83dda3ce3215058011fc1d41d9f4aec9071ca8b1
Petter Holmstrom <petter.holmstrom@abo.fi>
2011-10-18 10:40:49 +0300

    Removed ThreadLocal, storing the username using Activiti only

--- simple-travel/src/main/java/org/vaadin/activiti/simpletravel/identity/CurrentUserFactoryBean.java
+++ simple-travel/src/main/java/org/vaadin/activiti/simpletravel/identity/CurrentUserFactoryBean.java
@@ -1,7 +1,6 @@
 package org.vaadin.activiti.simpletravel.identity;
 
-import org.activiti.engine.IdentityService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.activiti.engine.impl.identity.Authentication;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
@@ -9,26 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CurrentUserFactoryBean {
-    
-    private final InheritableThreadLocal<String> username = new InheritableThreadLocal<String>();
-    
-    @Autowired
-    protected transient IdentityService identityService;
-    
-    
+        
     @Bean
     @Qualifier("currentUsername")
     @Scope("prototype")
     public String getCurrentUsername() {
-        return username.get();
+        return Authentication.getAuthenticatedUserId();
     }
     
     public void setCurrentUsername(String username) {
-        if (username == null) {
-            this.username.remove();
-        } else {
-            this.username.set(username);
-        }
-        identityService.setAuthenticatedUserId(username);
+        Authentication.setAuthenticatedUserId(username);
     }
 }

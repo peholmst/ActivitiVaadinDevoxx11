ea9a8ba534394f9a65de5b19947253c94c8a1dcd
Frederik Heremans <frederik.heremans@gmail.com>
2011-10-18 09:29:02 +0200

    Implemented rejection test path

--- simple-travel/src/test/java/org/vaadin/activiti/simpletravel/test/TravelProcessTest.java
+++ simple-travel/src/test/java/org/vaadin/activiti/simpletravel/test/TravelProcessTest.java
@@ -3,6 +3,7 @@ package org.vaadin.activiti.simpletravel.test;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.activiti.engine.FormService;
 import org.activiti.engine.RepositoryService;
@@ -10,6 +11,7 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.form.FormProperty;
 import org.activiti.engine.form.StartFormData;
+import org.activiti.engine.impl.identity.Authentication;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
@@ -21,12 +23,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:application-context-process.xml",
 		"classpath:test-application-context-process.xml" })
 public class TravelProcessTest {
 
+	private static final Logger LOGGER = Logger.getLogger(TravelProcessTest.class.getName());
+	
 	@Autowired
 	private RuntimeService runtimeService;
 
@@ -42,10 +48,13 @@ public class TravelProcessTest {
 	@Autowired
 	@Rule
 	public ActivitiRule activitiSpringRule;
+	
+	@Autowired
+	public Wiser wiserServer;
 
 	@Test
 	@Deployment(resources = "process/simple-travel.bpmn20.xml")
-	public void testTravelProcessRejectRequest() {
+	public void testTravelProcessRejectRequest() throws Exception {
 		// Get hold of the ID of our latest process
 		String processDefinitionId = repositoryService
 				.createProcessDefinitionQuery()
@@ -64,9 +73,10 @@ public class TravelProcessTest {
 			Assert.assertTrue(properties.containsKey(prop.getId()));
 		}
 
-		// Start the actual process
+		// Start the actual process as a traveler
+		Authentication.setAuthenticatedUserId("traveller");
 		ProcessInstance processInstance = runtimeService
-				.startProcessInstanceByKey("simple-travel");
+				.startProcessInstanceByKey("simple-travel", properties);
 		Assert.assertNotNull(processInstance);
 
 		// Check if a task is available, queued for a manager
@@ -76,10 +86,29 @@ public class TravelProcessTest {
 		Assert.assertEquals(processInstance.getId(),
 				theTask.getProcessInstanceId());
 
+		// Claim and finish the task as manager
+		Authentication.setAuthenticatedUserId("manager");
+		taskService.claim(theTask.getId(), "manager");
+		
 		// Reject the request
 		Map<String, Object> rejectProperties = getRejectProperties();
-		// TODO: implement
-
+		taskService.complete(theTask.getId(), rejectProperties);
+		
+		Assert.assertEquals(1, wiserServer.getMessages().size());
+		WiserMessage rejectionMail = wiserServer.getMessages().get(0);
+		wiserServer.getMessages().clear();
+		
+		// Check the mail's from and to
+		Assert.assertEquals("traveller@vaadin-activiti.org", rejectionMail.getEnvelopeReceiver());
+		Assert.assertEquals("no-reply@vaadin-activiti.org", rejectionMail.getEnvelopeSender());
+		String data = new String(rejectionMail.getData(), "UTF-8");
+		
+		LOGGER.info("Recieved mail: \n" + data);
+		
+		// Check mail body for receiver, rejecting manager and motivation
+		Assert.assertTrue(data.contains("Dear traveller"));
+		Assert.assertTrue(data.contains("Test Rejection"));
+		Assert.assertTrue(data.contains("by manager"));
 	}
 
 	protected Map<String, Object> getStartProperties() {

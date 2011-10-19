c11585c8496153f5bf4e2243033861213c7eed88
Petter Holmstrom <petter.holmstrom@abo.fi>
2011-10-19 14:37:03 +0300

    TaskFormMap moved to different package

--- simple-travel/src/main/java/org/vaadin/activiti/simpletravel/ui/forms/impl/TaskFormMap.java
+++ simple-travel/src/main/java/org/vaadin/activiti/simpletravel/ui/forms/impl/TaskFormMap.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.vaadin.activiti.simpletravel.ui.forms;
+package org.vaadin.activiti.simpletravel.ui.forms.impl;
 
 /**
  *

1d06b4f03513168b1e50f4d1f7a41a3ee3f9d759
Frederik Heremans <frederik.heremans@gmail.com>
2011-11-07 14:25:34 +0100

    Fixed issue with validation annotation

--- simple-travel/src/main/java/org/vaadin/activiti/simpletravel/domain/validation/ReturnsAfterDeparture.java
+++ simple-travel/src/main/java/org/vaadin/activiti/simpletravel/domain/validation/ReturnsAfterDeparture.java
@@ -5,12 +5,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = ReturnsAfterDepartureValidator.class)
+@Constraint(validatedBy=ReturnsAfterDepartureValidator.class)
 @Documented
 public @interface ReturnsAfterDeparture {
     
